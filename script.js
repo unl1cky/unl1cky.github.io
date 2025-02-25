c3470e47e7ecb97292c3672bc3ea01218c7fd48a
unl1cky <111482580+unl1cky@users.noreply.github.com>
2025-02-25 12:28:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function toggleLanguage() {
         document.getElementById("intro").innerText = "Привет, я Димаш, я играю вартандар .";
 
         document.getElementById("about-title").innerText = "Обо мне";
-        document.getElementById("about-text").innerText = "Привет! Я увлекаюсь дебатами. Я специализируюсь в английском. В свободное время мне нравится вартандар.";
+        document.getElementById("about-text").innerText = "Привет! Я увлекаюсь дебатами sigma. Я специализируюсь в английском. В свободное время мне нравится вартандар.";
 
         document.getElementById("projects-title").innerText = "Мои проекты";
         document.getElementById("project1").innerText = "сигма.";
@@ -22,21 +22,21 @@ function toggleLanguage() {
         document.getElementById("translate-btn").innerText = "Translate to English";
     } else {
         document.getElementById("title").innerText = "Welcome to My Website";
-        document.getElementById("intro").innerText = "Hi, I'm [Your Name], a [Your Profession or Interest].";
+        document.getElementById("intro").innerText = "Hi, I'm Dimash, a debater.";
 
         document.getElementById("about-title").innerText = "About Me";
-        document.getElementById("about-text").innerText = "Hello! I'm passionate about [your interests]. I specialize in [your skills]. In my free time, I enjoy [your hobbies].";
+        document.getElementById("about-text").innerText = "Hello! I'm passionate about debates. I specialize in English. In my free time, I enjoy War Thunder.";
 
         document.getElementById("projects-title").innerText = "My Projects";
-        document.getElementById("project1").innerText = "Description of your project.";
-        document.getElementById("project2").innerText = "Description of your project.";
-        document.getElementById("project3").innerText = "Description of your project.";
+        document.getElementById("project1").innerText = "Sigma.";
+        document.getElementById("project2").innerText = "Alpha.";
+        document.getElementById("project3").innerText = "Sugma.";
 
         document.getElementById("contact-title").innerText = "Contact Me";
-        document.getElementById("email").innerText = "Email: your.email@example.com";
-        document.getElementById("github").innerHTML = "GitHub: <a href='https://github.com/yourusername' target='_blank'>github.com/yourusername</a>";
+        document.getElementById("email").innerText = "Email: dnur5345@gmail.com";
+        document.getElementById("github").innerHTML = "GitHub: <a href='https://github.com/unl1cky' target='_blank'>github.com/unl1cky</a>";
 
-        document.getElementById("footer-text").innerText = "© 2025 [Your Name]. All rights reserved.";
+        document.getElementById("footer-text").innerText = "© 2025 Dimash. All rights reserved.";
 
         document.getElementById("translate-btn").innerText = "Translate to Russian";
     }
